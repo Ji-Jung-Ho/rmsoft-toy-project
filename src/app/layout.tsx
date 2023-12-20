cf32607d7f3c11f7feec0afde083eecc2910f63b
DESKTOP-SMD51TC\kiik5 <kiik52@naver.com>
2023-12-20 21:45:32 +0900

    메모중복생성방지, 메모유무화면구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
